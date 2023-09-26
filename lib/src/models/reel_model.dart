8469e8f0b114ff441fd7d45c6a8e8ff98346e783
Jigar Thakkar <jigar1859@gmail.com>
2023-09-26 09:27:24 +0530

    Object serializer for ReelMoidel

--- lib/src/models/reel_model.dart
+++ lib/src/models/reel_model.dart
@@ -10,5 +10,32 @@ class ReelModel {
   final String? reelDescription;
   final String? musicName;
   final List<ReelCommentModel>? commentList;
-  ReelModel(this.url,this.userName,{this.id,this.isLiked =false,this.likeCount=0,this.profileUrl,this.reelDescription,this.musicName,this.commentList});
+  ReelModel(this.url, this.userName,
+      {this.id,
+      this.isLiked = false,
+      this.likeCount = 0,
+      this.profileUrl,
+      this.reelDescription,
+      this.musicName,
+      this.commentList});
+
+  ReelModel.fromJson(
+      Map<String, dynamic> json, this.likeCount, this.commentList)
+      : id = json['id'],
+        url = json['url'],
+        isLiked = json['isLiked'],
+        userName = json['userName'],
+        profileUrl = json['profileUrl'],
+        reelDescription = json['reelDescription'],
+        musicName = json['musicName'];
+
+  Map<String, dynamic> toJson() => {
+        'id': id,
+        'url': url,
+        'isLiked': isLiked,
+        'userName': userName,
+        'profileUrl': profileUrl,
+        'reelDescription': reelDescription,
+        'musicName': musicName
+      };
 }
